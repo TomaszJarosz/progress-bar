6e62f98832c81251da2b1723f71c1bdf21587b09
tomaszjarosz <tomaszjarosz1994@gmail.com>
2023-12-29 21:11:10 +0100

    step2: use types

--- trait-example/src/main.rs
+++ trait-example/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
 }
 
 mod step1;
+mod step2;
 
 #[test]
 fn test_step_1() {
@@ -23,4 +24,24 @@ fn test_step_1() {
     assert!(balances.transfer(1,2,100).is_err());
     assert_eq!(balances.balance(1), 50);
     assert_eq!(balances.balance(2), 250);
+}
+
+#[test]
+fn test_step_2() {
+    let mut balances = step2::BalanceModule::new();
+    balances.set_balances(1, 100);
+    balances.set_balances(2, 200);
+
+    assert_eq!(balances.balance(1), 100);
+    assert_eq!(balances.balance(2), 200);
+    assert_eq!(balances.balance(3), 0);
+
+    assert!(balances.transfer(1,2,50).is_ok());
+    assert_eq!(balances.balance(1), 50);
+    assert_eq!(balances.balance(2), 250);
+
+    assert!(balances.transfer(1,2,100).is_err());
+    assert_eq!(balances.balance(1), 50);
+    assert_eq!(balances.balance(2), 250);
+
 }
