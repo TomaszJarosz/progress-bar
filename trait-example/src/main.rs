a7d5bf3d55ed66fb8fb3ccf2adc39ee51f66e55c
tomaszjarosz <tomaszjarosz1994@gmail.com>
2023-12-29 23:41:03 +0100

    step 4: use generics

--- trait-example/src/main.rs
+++ trait-example/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
 mod step1;
 mod step2;
 mod step3;
+mod step4;
 
 #[test]
 fn test_step_1() {
@@ -18,11 +19,11 @@ fn test_step_1() {
     assert_eq!(balances.balance(2), 200);
     assert_eq!(balances.balance(3), 0);
 
-    assert!(balances.transfer(1,2,50).is_ok());
+    assert!(balances.transfer(1, 2, 50).is_ok());
     assert_eq!(balances.balance(1), 50);
     assert_eq!(balances.balance(2), 250);
 
-    assert!(balances.transfer(1,2,100).is_err());
+    assert!(balances.transfer(1, 2, 100).is_err());
     assert_eq!(balances.balance(1), 50);
     assert_eq!(balances.balance(2), 250);
 }
@@ -37,12 +38,28 @@ fn test_step_2() {
     assert_eq!(balances.balance(2), 200);
     assert_eq!(balances.balance(3), 0);
 
-    assert!(balances.transfer(1,2,50).is_ok());
+    assert!(balances.transfer(1, 2, 50).is_ok());
     assert_eq!(balances.balance(1), 50);
     assert_eq!(balances.balance(2), 250);
 
-    assert!(balances.transfer(1,2,100).is_err());
+    assert!(balances.transfer(1, 2, 100).is_err());
     assert_eq!(balances.balance(1), 50);
     assert_eq!(balances.balance(2), 250);
-
 }
+
+#[test]
+fn test_step_4() {
+    type AccountId = u16;
+    type VoteIndex = u64;
+    let user_1 = 1;
+    let user_2 = 2;
+    let mut balances = step4::BalanceModule::<AccountId, VoteIndex>::new();
+    let mut voting = step4::VotingModule::<AccountId, VoteIndex>::new();
+
+    balances.set_balances(user_1, 100);
+    balances.set_balances(user_2, 200);
+
+    voting.vote(user_1, 0, true);
+    voting.vote(user_2, 0, true);
+}
+
