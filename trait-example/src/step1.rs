908565c3bc442e6daa8f3d43656851be969b4f45
tomaszjarosz <tomaszjarosz1994@gmail.com>
2023-12-29 21:06:02 +0100

    BalanceModule: allow transfers

--- trait-example/src/step1.rs
+++ trait-example/src/step1.rs
@@ -18,4 +18,16 @@ impl BalanceModule {
     pub fn balance(&self, who: u32) -> u32 {
         *self.balances.get(&who).unwrap_or(&0)
     }
+
+    pub fn transfer(&mut self, from: u32, to: u32, amount: u32) -> Result<(), &str> {
+        let from_balance = self.balances.get(&from).ok_or("from user does not exist")?;
+        let to_balance = self.balances.get(&to).unwrap_or(&0);
+
+        let new_from_balance = from_balance.checked_sub(amount).ok_or("Not enough funds")?;
+        let new_to_balance = to_balance.checked_add(amount).ok_or("Overflow")?;
+
+        self.balances.insert(from, new_from_balance);
+        self.balances.insert(to, new_to_balance);
+        Ok(())
+    }
 }
