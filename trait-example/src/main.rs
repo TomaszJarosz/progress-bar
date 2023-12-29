34c28944785fb1b248266908e7c9f39a49853af8
tomaszjarosz <tomaszjarosz1994@gmail.com>
2023-12-29 23:57:47 +0100

    step 5: bundle all types into single Trait

--- trait-example/src/main.rs
+++ trait-example/src/main.rs
@@ -1,5 +1,8 @@
 //https://www.youtube.com/watch?v=6cp10jVWNl4 Substrate Seminar - Traits and Generic Types
 
+use std::hash::Hash;
+use num::{CheckedAdd, CheckedSub, Zero};
+
 fn main() {
     println!("Hello, world!");
 }
@@ -8,6 +11,7 @@ mod step1;
 mod step2;
 mod step3;
 mod step4;
+mod step5;
 
 #[test]
 fn test_step_1() {
@@ -55,6 +59,32 @@ fn test_step_4() {
     let user_2 = 2;
     let mut balances = step4::BalanceModule::<AccountId, VoteIndex>::new();
     let mut voting = step4::VotingModule::<AccountId, VoteIndex>::new();
+
+    balances.set_balances(user_1, 100);
+    balances.set_balances(user_2, 200);
+
+    voting.vote(user_1, 0, true);
+    voting.vote(user_2, 0, true);
+}
+
+trait Trait {
+    type AccountId: Eq + Hash;
+    type Balance: Zero + CheckedAdd + CheckedSub + Copy;
+    type VoteIndex: Eq + Hash;
+}
+
+#[test]
+fn test_step_5() {
+    struct Runtime;
+    impl Trait for Runtime {
+        type AccountId = u32;
+        type Balance = u32;
+        type VoteIndex = u8;
+    }
+    let user_1 = 1;
+    let user_2 = 2;
+    let mut balances = step5::BalanceModule::<Runtime>::new();
+    let mut voting = step5::VotingModule::<Runtime>::new();
 
     balances.set_balances(user_1, 100);
     balances.set_balances(user_2, 200);
